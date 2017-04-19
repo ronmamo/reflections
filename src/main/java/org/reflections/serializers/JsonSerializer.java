802be641559a56e51b85dffe6737a15b62e1db55
Victor Cordis <cordis.victor@gmail.com>
2017-04-19 07:25:45 +0300

    issue160: refactor Gson usage

--- src/main/java/org/reflections/serializers/JsonSerializer.java
+++ src/main/java/org/reflections/serializers/JsonSerializer.java
@@ -15,9 +15,11 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
-/** serialization of Reflections to json
+/**
+ * Serialization of Reflections to json.
  *
- * <p>an example of produced json:
+ * <p>
+ * an example of produced json:
  * <pre>
  * {"store":{"storeMap":
  *    {"org.reflections.scanners.TypeAnnotationsScanner":{
@@ -25,14 +27,43 @@ import java.util.Set;
  *       "org.reflections.TestModel$AC2":["org.reflections.TestModel$I3",
  * ...
  * </pre>
- * */
+ *
+ */
 public class JsonSerializer implements Serializer {
-    private Gson gson;
 
+    private static final Gson gson = new GsonBuilder()
+            .registerTypeAdapter(Multimap.class, new com.google.gson.JsonSerializer<Multimap>() {
+                @Override
+                public JsonElement serialize(Multimap multimap, Type type, JsonSerializationContext jsonSerializationContext) {
+                    return jsonSerializationContext.serialize(multimap.asMap());
+                }
+            })
+            .registerTypeAdapter(Multimap.class, new JsonDeserializer<Multimap>() {
+                @Override
+                public Multimap deserialize(JsonElement jsonElement, Type type, JsonDeserializationContext jsonDeserializationContext) throws JsonParseException {
+                    final SetMultimap<String, String> map = Multimaps.newSetMultimap(new HashMap<String, Collection<String>>(), new Supplier<Set<String>>() {
+                        @Override
+                        public Set<String> get() {
+                            return Sets.newHashSet();
+                        }
+                    });
+                    for (Map.Entry<String, JsonElement> entry : ((JsonObject) jsonElement).entrySet()) {
+                        for (JsonElement element : (JsonArray) entry.getValue()) {
+                            map.get(entry.getKey()).add(element.getAsString());
+                        }
+                    }
+                    return map;
+                }
+            })
+            .setPrettyPrinting()
+            .create();
+
+    @Override
     public Reflections read(InputStream inputStream) {
-        return getGson().fromJson(new InputStreamReader(inputStream), Reflections.class);
+        return gson.fromJson(new InputStreamReader(inputStream), Reflections.class);
     }
 
+    @Override
     public File save(Reflections reflections, String filename) {
         try {
             File file = Utils.prepareFile(filename);
@@ -43,37 +74,9 @@ public class JsonSerializer implements Serializer {
         }
     }
 
+    @Override
     public String toString(Reflections reflections) {
-        return getGson().toJson(reflections);
-    }
-
-    private Gson getGson() {
-        if (gson == null) {
-            gson = new GsonBuilder()
-                    .registerTypeAdapter(Multimap.class, new com.google.gson.JsonSerializer<Multimap>() {
-                        public JsonElement serialize(Multimap multimap, Type type, JsonSerializationContext jsonSerializationContext) {
-                            return jsonSerializationContext.serialize(multimap.asMap());
-                        }
-                    })
-                    .registerTypeAdapter(Multimap.class, new JsonDeserializer<Multimap>() {
-                        public Multimap deserialize(JsonElement jsonElement, Type type, JsonDeserializationContext jsonDeserializationContext) throws JsonParseException {
-                            final SetMultimap<String,String> map = Multimaps.newSetMultimap(new HashMap<String, Collection<String>>(), new Supplier<Set<String>>() {
-                                public Set<String> get() {
-                                    return Sets.newHashSet();
-                                }
-                            });
-                            for (Map.Entry<String, JsonElement> entry : ((JsonObject) jsonElement).entrySet()) {
-                                for (JsonElement element : (JsonArray) entry.getValue()) {
-                                    map.get(entry.getKey()).add(element.getAsString());
-                                }
-                            }
-                            return map;
-                        }
-                    })
-                    .setPrettyPrinting()
-                    .create();
-
-        }
-        return gson;
+        return gson.toJson(reflections);
     }
 }
+
