d02ba767a3a115c68de88a7abc4399a9249b1666
Sébastien Lepage <sebastien.lepage@outlook.com>
2017-11-05 18:20:58 +0100

    Fix #205 - Reflections.scan allow too many potential result

--- src/main/java/org/reflections/Reflections.java
+++ src/main/java/org/reflections/Reflections.java
@@ -249,7 +249,7 @@ public class Reflections {
                     Object classObject = null;
                     for (Scanner scanner : configuration.getScanners()) {
                         try {
-                            if (scanner.acceptsInput(path) || scanner.acceptResult(fqn)) {
+                            if (scanner.acceptsInput(path) || scanner.acceptsInput(fqn)) {
                                 classObject = scanner.scan(file, classObject);
                             }
                         } catch (Exception e) {
