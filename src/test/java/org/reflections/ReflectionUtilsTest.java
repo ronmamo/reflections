481f21f73ee9a0ea6ede79ae9bd4864a9a334a33
ronma <ronma@>
2020-04-26 21:10:02 +0700

    fix custom url class loader test

--- src/test/java/org/reflections/ReflectionUtilsTest.java
+++ src/test/java/org/reflections/ReflectionUtilsTest.java
@@ -151,7 +151,7 @@ public class ReflectionUtilsTest {
 
     public static String toStringSorted(Set<?> set) {
         return set.stream()
-                .map(o -> o.toString().replace("[]", "").replace("]", "").replace("{", "").replace("}", "").replace("\"", ""))
+                .map(o -> o.toString().replace("[", "").replace("]", "").replace("{", "").replace("}", "").replace("\"", ""))
                 .sorted().collect(Collectors.toList()).toString();
     }
 }
